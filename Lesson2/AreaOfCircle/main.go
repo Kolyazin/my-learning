5493a08cfe583422da936e9c08fbbf0046b5d752
Sergey Kolyazin <kolyazin.se@gmail.com>
2022-02-17 00:12:24 +0300

    Добавлена программа вычисления площади прямоугольника. В программу вычисления площади круга добавлена обработка ошибок ввода.

--- Lesson2/AreaOfCircle/main.go
+++ Lesson2/AreaOfCircle/main.go
@@ -9,6 +9,10 @@ func main() {
 	var radius float64
 
 	fmt.Print("Введите радиус круга: ")
-	fmt.Scanln(&radius)
-	fmt.Println("Площадь круга = ", math.Pi*radius*radius)
+	_, err := fmt.Scanln(&radius)
+	if err != nil {
+		fmt.Println("Введено неверное значение. Попробуйте еще раз.")
+	} else {
+		fmt.Println("Площадь круга = ", math.Pi*radius*radius)
+	}
 }
