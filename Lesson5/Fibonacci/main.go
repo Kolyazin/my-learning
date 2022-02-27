17112c7d89ccaaca875e42fd0d58609eff554dbb
sk <ya.sk10@yanswx.ru>
2022-02-27 19:01:59 +0300

    Реализована программа вычисляющая число Фибоначчи. Попробовал развернуть и отсортировать односвязный список.

---
+++ Lesson5/Fibonacci/main.go
@@ -0,0 +1,29 @@
+package main
+
+import "fmt"
+
+func fibonacci(n int) int {
+	if n == 0 || n == 1 {
+		return n
+	} else {
+		return fibonacci(n-1) + fibonacci(n-2)
+	}
+}
+
+func fibonacciOptimized(n int) int {
+	fi := make(map[int]int)
+	fi[0] = 0
+	fi[1] = 1
+	for i := 2; i <= n; i++ {
+		fi[i] = fi[i-1] + fi[i-2]
+	}
+	return fi[n]
+}
+
+func main() {
+	var n int
+	fmt.Printf("Ведите число для которого нужно вычислить число Фибоначчи: ")
+	fmt.Scanln(&n)
+	fmt.Println(fibonacci(n))
+	fmt.Println(fibonacciOptimized(n))
+}
