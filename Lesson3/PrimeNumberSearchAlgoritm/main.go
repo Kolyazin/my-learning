66e8d5bd7e252ed6c422f83b0ed14e3c333aa9f1
sk <ya.sk10@yanswx.ru>
2022-03-02 02:21:01 +0300

    Переделал функцию нахождения простых чисел. Теперь больше похожа на решето Эратосфена. В Eratosthenes2 сделал через map. Использовал функцию сортировки из четвертого домашнего задания.

--- Lesson3/PrimeNumberSearchAlgoritm/main.go
+++ Lesson3/PrimeNumberSearchAlgoritm/main.go
@@ -16,24 +16,57 @@ import (
 )
 
 func Eratosthenes(n int) []int {
-	//итоговый массив простых чисел
-	var primes []int
-	for i := 2; i <= n; i++ {
-		// flag = true означает простое число
-		isPrime := true
-		for j := 2; j < i; j++ {
-			if i%j == 0 {
-				// если число поделилось без остатка на другое, то это уже не простое, его не добавляем, и переходим к следующему.
-				isPrime = false
-				break
-			}
-		}
-		if isPrime { // если flag == true значит число простое, его добавляем в итоговый слайс
-			primes = append(primes, i)
-		}
+	//итоговый и начальный массив простых чисел
+	var primes, base []int
+	for i := 0; i <= n; i++ {
+		base = append(base, i)
 	}
 
+	for i := 2; i <= n; i++ {
+		// вычеркиваю числа только кратные i
+		for j := i + i; j <= n; j += i {
+			base[j] = 0
+		}
+	}
+	primes = append(primes, 0)
+	for i := 1; i <= n; i++ {
+		if base[i] != 0 {
+			primes = append(primes, base[i])
+		}
+	}
 	return primes
+}
+
+func Eratosthenes2(n int) []int {
+	//итоговый и начальный массив простых чисел
+	var primes []int
+	base := map[int]int{}
+	for i := 0; i <= n; i++ {
+		base[i] = i
+	}
+
+	for i := 2; i <= n; i++ {
+		// вычеркиваю числа только кратные i
+		for j := i + i; j <= n; j += i {
+			delete(base, j)
+		}
+	}
+	for i := range base {
+		primes = append(primes, i)
+	}
+
+	insertionSort(primes)
+
+	return primes
+}
+
+// Вот и пригодилась функция сортировки вставкой из четвертой домашней работы)
+func insertionSort(slice []int) {
+	for i := 1; i < len(slice); i++ {
+		for j := i; j > 0 && slice[j-1] > slice[j]; j-- {
+			slice[j], slice[j-1] = slice[j-1], slice[j]
+		}
+	}
 }
 
 func main() {
@@ -45,5 +78,6 @@ func main() {
 		return
 	}
 	fmt.Println("Простые числа: ", Eratosthenes(number))
+	fmt.Println("Простые числа: ", Eratosthenes2(number))
 
 }
