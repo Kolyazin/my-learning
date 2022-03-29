577cda6e42c36c2f4c2730bd750ccee4b1483087
sk <ya.sk10@yanswx.ru>
2022-03-29 14:42:46 +0300

    Переделал на отдельные переменные.

--- Lesson8/readconfig/readconfig.go
+++ Lesson8/readconfig/readconfig.go
@@ -7,50 +7,60 @@ import (
 	"net/url"
 )
 
-type Config struct {
+type RawConfig struct {
 	Port int
 	Db_url string
-	Db_url_parsed *url.URL
 	Jaeger_url string
-	Jaeger_url_parsed *url.URL
 	Sentry_url string
-	Sentry_url_parsed *url.URL
 	Kafka_broker string
+	Some_app_id string
+	Some_app_key string
+}
+
+type ValidConfig struct {
+	Port int
+	Db_url_parsed *url.URL
+	Jaeger_url_parsed *url.URL
+	Sentry_url_parsed *url.URL
 	Kafka_broker_parsed *url.URL
 	Some_app_id string
 	Some_app_key string
 }
 
 var (
-	Conf Config
+	RawConf RawConfig
+	ValidConf ValidConfig
 )
 
 
 func init() {
-	flag.IntVar(&Conf.Port, "port", 8080, "port")                                                                                //: 8080
-	flag.StringVar(&Conf.Db_url, "db_url", "postgres://db-user:db-password@petstore-db:5432/petstore?sslmode=disable", "db_url") //: postgres://db-user:db-password@petstore-db:5432/petstore?sslmode=disable
-	flag.StringVar(&Conf.Jaeger_url, "jaeger_url", "http://jaeger:16686", "jaeger")                                              //: http://jaeger:16686
-	flag.StringVar(&Conf.Sentry_url, "sentry_url", "http://sentry:9000", "sentry_url")                                           //: http://sentry:9000
-	flag.StringVar(&Conf.Kafka_broker, "kafka_broker", "kafka:9092", "kafka_broker")                                             //: kafka:9092
-	flag.StringVar(&Conf.Some_app_id, "some_app_id", "DefaultId", "some_app_id")                                                    //: testid
-	flag.StringVar(&Conf.Some_app_key, "some_app_key", "DefaultKey", "some_app_key")                                                //: testkey
+	flag.IntVar(&RawConf.Port, "port", 8080, "port")                                                                                //: 8080
+	flag.StringVar(&RawConf.Db_url, "db_url", "postgres://db-user:db-password@petstore-db:5432/petstore?sslmode=disable", "db_url") //: postgres://db-user:db-password@petstore-db:5432/petstore?sslmode=disable
+	flag.StringVar(&RawConf.Jaeger_url, "jaeger_url", "http://jaeger:16686", "jaeger")                                              //: http://jaeger:16686
+	flag.StringVar(&RawConf.Sentry_url, "sentry_url", "http://sentry:9000", "sentry_url")                                           //: http://sentry:9000
+	flag.StringVar(&RawConf.Kafka_broker, "kafka_broker", "kafka:9092", "kafka_broker")                                             //: kafka:9092
+	flag.StringVar(&RawConf.Some_app_id, "some_app_id", "DefaultId", "some_app_id")                                                    //: testid
+	flag.StringVar(&RawConf.Some_app_key, "some_app_key", "DefaultKey", "some_app_key")                                                //: testkey
 }
 
-func ReadConfig() Config {
+func ReadConfig() ValidConfig {
 	flag.Parse()
 
 	if flag.NFlag() == 0 {
 //если флагов нет, то беру из переменных окружения
-		err := envconfig.Process("", &Conf)
+		err := envconfig.Process("", &RawConf)
 		if err != nil {
 			fmt.Println(err.Error())
 		}
 	}
 //валидирую url
-	Conf.Db_url_parsed, _ = url.Parse(Conf.Db_url)
-	Conf.Jaeger_url_parsed, _ = url.Parse(Conf.Jaeger_url)
-	Conf.Sentry_url_parsed, _ = url.Parse(Conf.Sentry_url)
-	Conf.Kafka_broker_parsed, _ = url.Parse(Conf.Kafka_broker)
+	ValidConf.Port = RawConf.Port
+	ValidConf.Db_url_parsed, _ = url.Parse(RawConf.Db_url)
+	ValidConf.Jaeger_url_parsed, _ = url.Parse(RawConf.Jaeger_url)
+	ValidConf.Sentry_url_parsed, _ = url.Parse(RawConf.Sentry_url)
+	ValidConf.Kafka_broker_parsed, _ = url.Parse(RawConf.Kafka_broker)
+	ValidConf.Some_app_id = RawConf.Some_app_id
+	ValidConf.Some_app_key = RawConf.Some_app_key
 
-	return Conf
+	return ValidConf
 }
